3ed2574e786182f3689d4eb76c874449c8fe6970
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-07 17:22:58 -0600

    Install react-router-dom, v6. Create places, user, & shared folders. Components folder in each of them. pages folder in user & places. Import createBrowserRouter, createRoutesFromElements, Route, & RouterProvider into index.js. Create router const, with createBrowserRouter & the createRoutesFromElements config. Set up the RouterProvider on the render with the router attribute using the router const. Set up root path with App element. Create a Users component func in user/pages. Create a NewPlace component func in places/pages. Set up a Users page as the element at the root path, and index being true. Set up a Route with /places/new path & NewPlace element.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
-import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+    <Route index={true} path="/" element={<Users />} />
+    <Route path="/places/new" element={<NewPlace />} />
+    </Route>
+  )
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
