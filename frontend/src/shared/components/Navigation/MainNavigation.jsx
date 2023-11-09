c72ad6396b4562759e4dc1c5237010ee1b786747
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-09 16:35:30 -0600

    Create func called closeDrawer that sets drawerIsOpen state back to false. Put Backdrop.css and Backdrop.js from course into shared/components/UIElements folder. Set div in index.html with "backdrop-hook" id. Import Backdrop into MainNavigation, and conditionally render if drawerIsOpen is true. Set onClick attribute on Backdrop and bind to closeDrawer func.

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -5,6 +5,7 @@ import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
@@ -13,8 +14,13 @@ const MainNavigation = (props) => {
     setDrawerIsOpen(true);
   }
 
+  const closeDrawer = () => {
+      setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
+    {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
     {drawerIsOpen && (<SideDrawer>
       <nav className="main-navigation__drawer-nav">
         <NavLinks />
