06c2698ce784fad553540ed6cc3ac6ba098a01e9
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-09 20:50:00 -0600

    Install react-transition-group. Import CSSTransition from react-transition-group into SideDrawer.jsx. Wrap the value of the content const in CSSTransition element. Configure CSSTransition elementr with in, timeout, classNames, mountOnEnter, unmountOnExit attributes.

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -7,6 +7,7 @@ import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
 import Backdrop from "../UIElements/Backdrop";
 
+
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
@@ -20,25 +21,26 @@ const MainNavigation = (props) => {
 
   return (
     <React.Fragment>
-    {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
-    {drawerIsOpen && (<SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavLinks />
-      </nav>
-    </SideDrawer>)}
-    <MainHeader>
-      <button className="main-navigation__menu-btn" onClick={openDrawer}>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Location Station!</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (<SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Location Station!</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
     </React.Fragment>
   );
 }
