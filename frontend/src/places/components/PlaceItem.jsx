26ce37366a75090b2d5fd74508dd72cc275a9124
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-11 14:20:22 -0600

    Wire the openMapHandler func to the VIEW ON MAP Button with onClick. Set up header, footer, and children props for Modal to show the map. Set placeholder text SHOW MAP! in h2 element. Create map-container CSS class in PlaceItem.css. Set event.preventDefault to func call with event.preventDefault() in Modal.jsx.

--- frontend/src/places/components/PlaceItem.jsx
+++ frontend/src/places/components/PlaceItem.jsx
@@ -7,7 +7,6 @@ import Card from "../../shared/components/UIElements/Card";
 import Modal from "../../shared/components/UIElements/Modal";
 
 const PlaceItem = (props) => {
-
   const [showMap, setShowMap] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
@@ -16,7 +15,18 @@ const PlaceItem = (props) => {
 
   return (
     <>
-    <Modal show={showMap} onCancel={closeMapHandler} />
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!</h2>
+        </div>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -28,7 +38,7 @@ const PlaceItem = (props) => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse>VIEW ON MAP</Button>
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
             <Button danger>DELETE</Button>
           </div>
@@ -36,6 +46,6 @@ const PlaceItem = (props) => {
       </li>
     </>
   );
-}
+};
 
-export default PlaceItem
+export default PlaceItem;
