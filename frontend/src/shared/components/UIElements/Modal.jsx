eb5477eb6ac644ec1ce36525b9fed1064a89ae05
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-11 15:27:54 -0600

    Format code in PlaceItem and Modal components. Wrap ModalOverlay render in return with a parent element div. Set the nodeRef prop and forwarding the ref to the top level DOM node in ModalOverlay component. This means passing ref as an argument in ModalOverlay component with the forwardRef method. In Modal component set nodeRef const to use useRef hook and pass in null. In CSSTransition element, set nodeRef prop to noderef. In ModalOverlay render, set ref prop to nodeRef const.

--- frontend/src/shared/components/UIElements/Modal.jsx
+++ frontend/src/shared/components/UIElements/Modal.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom"; // This is a package that allows us to render our react app to the DOM.
 import { CSSTransition } from "react-transition-group";
 
-
 import "./Modal.css";
 import Backdrop from "./Backdrop";
 
-const ModalOverlay = (props) => {
-
+const ModalOverlay = React.forwardRef((props, ref) => {
   // This is the content that will be rendered to the DOM when the Modal component is called in another component.
   const content = (
-    <div className={`modal $(props.className)`} style={props.style}>
+    <div ref={ref} className={`modal $(props.className)`} style={props.style}>
       <header className={`modal__header ${props.headerClass}`}>
-        {" "}
         {/* This is the header of the modal.*/}
         <h2>{props.header}</h2>
       </header>
@@ -32,17 +29,31 @@ const ModalOverlay = (props) => {
   );
 
   return ReactDOM.createPortal(content, document.getElementById("modal-hook")); // This is the modal-hook div in index.html.
-};
+});
 
 const Modal = (props) => {
+
+  const nodeRef = React.useRef(null); // This is a reference to the ModalOverlay component that we created above.
+
   return (
     <>
-      {props.show && <Backdrop onClick={props.onCancel} />} {/* This is a ternary operator that checks if props.show is true. If it is, then it renders the Backdrop component. */}
-      <CSSTransition in={props.show} mountOnEnter unmountOnExit timeout={200} className="modal">
-        <ModalOverlay {...props} /> {/* This is a spread operator that takes all the key-value pairs of the props object, and spreads them all as attributes to the ModalOverlay component. */}
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      {/* This is a ternary operator that checks if props.show is true. If it is, then it renders the Backdrop component. */}
+      <CSSTransition
+        nodeRef={nodeRef}
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <div>
+          <ModalOverlay ref={nodeRef} {...props} />
+          {/* This is a spread operator that takes all the key-value pairs of the props object, and spreads them all as attributes to the ModalOverlay component. */}
+        </div>
       </CSSTransition>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
