4a9adba7602d3577da595554188c5251077e972e
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-12 20:37:04 -0600

    Add comments to Input component. Create changeHandler func in Input component to handle input changes. Add validators and error message to Title input field. Conditionally render an errorText prop if isValid is false. Set useReducer hook in Input component with reducer func to manage the input field state with a switch statement to check the type property of the action object. If the action type is `"CHANGE"`, a new state object is returned with a state spread operator to create a copy of the current state object, the value property set to the val property of the action object and isValid set to true. If no change, then the current unchanged state is returned. This is the default. Dispatch it in the changeHandler func, action type is CHANGE, and val property set to target.value.

--- frontend/src/shared/components/FormElements/Input.jsx
+++ frontend/src/shared/components/FormElements/Input.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./Input.css";
 
-const Input = (props) => {
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
 
-  const element = props.element === "input" ? <input id={props.id} type={props.type} placeholder={props.placeholder} /> : <textarea id={props.id} rows={props.rows || 3} />;
+const Input = (props) => {
+  // useReducer state initialization.
+  // useReducer returns an array with 2 elements: state and dispatch function (which is used to dispatch actions to the reducer function).
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
+
+  const changeHandler = (event) => {
+    console.log(event.target.value);
+    dispatch({ type: "CHANGE", val: event.target.value });
+  };
+
+  const element =
+    props.element === "input" ? (
+      // if props.element is "input", then use input tag with props.type and props.placeholder as attributes.
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      /> // if props.rows is undefined, then use 3 as default.
+    ); // if props.element is not "input", then it must be "textarea".
 
   return (
-    <div className={`form-control`}>
+    <div className={`form-control ${!inputState.isValid && "form-control--invalid"}`}>
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
