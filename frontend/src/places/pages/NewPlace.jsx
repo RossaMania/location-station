6d408133538d44511a1c733f06ca6bc810884206
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-14 16:35:46 -0600

    Import useCallback & useReducer hooks from React into form-hook. Remove useReducer hook from NewPlace component and create a separate useForm hook. Make initialInputs a parameter in the hook to handle the input initialization logic, & assign to inputs initial state. Make initialFormValidity another parameter in hook to handle form validity, & assign to isValid initial state. Cut formReducer from NewPlace & paste into form-hook outside of hook func.

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from "react";
+import React from "react";
 
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/utils/validators.jsx";
 import Input from "../../shared/components/FormElements/Input";
@@ -6,53 +6,8 @@ import Button from "../../shared/components/FormElements/Button";
 
 import "./PlaceForm.css";
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case "INPUT_CHANGE":
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          // if inputId is equal to action.inputId, this is the input the user changed. Check if action.isValid is true.
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          // if inputId is not equal to action.inputId, this is the other inputs in the form.
-          // The user didn't change these values. Check if state.inputs[inputId].isValid is true.
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          // action.inputId is the input ("title", "description", etc.) that is being changed.
-          [action.inputId]: { value: action.value, isValid: action.isValid },
-        },
-        isValid: formIsValid, // formIsValid is either true or false.
-      };
-      default:
-        return state;
-  }
-};
 
 const NewPlace = () => {
-
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
-      title: {
-        value: "",
-        isValid: false,
-      },
-      description: {
-        value: "",
-        isValid: false,
-      },
-      address: {
-        value: "",
-        isValid: false,
-      },
-    },
-    isValid: false,
-  });
 
   const inputHandler = useCallback((id, value, isValid) => {
     dispatch({type: "INPUT_CHANGE", inputId: id, value: value, isValid: isValid});
--- frontend/src/shared/hooks/form-hook.jsx
+++ frontend/src/shared/hooks/form-hook.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useCallback, useReducer } from 'react'
 
-const formHook = () => {
-  return (
-    <div>formHook</div>
-  )
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          // if inputId is equal to action.inputId, this is the input the user changed. Check if action.isValid is true.
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          // if inputId is not equal to action.inputId, this is the other inputs in the form.
+          // The user didn't change these values. Check if state.inputs[inputId].isValid is true.
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          // action.inputId is the input ("title", "description", etc.) that is being changed.
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid, // formIsValid is either true or false.
+      };
+    default:
+      return state;
+  }
+};
+
+
+export const useForm = (initialInputs, initialFormValidity) => {
+
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValidity,
+  });
 }
-
-export default formHook
