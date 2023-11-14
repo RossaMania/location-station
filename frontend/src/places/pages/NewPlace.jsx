33f03e4bd1b9f2efe10ffd3f359cf42344307934
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-13 20:55:21 -0600

    Add formReducer & useReducer hook to NewPlace component. In formReducer set a switch statement to handle a default case, and an "INPUT_CHANGE" action. The formReducer first initializes formIsValid to true, then iterates over all the input fields in the form (“state.inputs”) & checks if each input field is valid with an if-else statement. It checks the input field that the user has just changed, by matching the inputId of the current input field to the inputId of the action, and checks the validity of the new value with action.isValid. If valid, formIsValid remains true. Else, formisValid is false. Then the current values of the other inputs in the form are checked. If current inputId doesn’t match inputId in the action, & the current value is valid, then formIsValid remains true. Else, formIsValid is false. After all this, a new state object is returned. It’s a copy of the old state (...state), the updated value & updated isValid properties of the input field the user just changed, and isValid property is updated to the value of formIsValid—either all the input fields in the form are valid, or not.

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,11 +1,53 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/utils/validators.jsx";
 import Input from "../../shared/components/FormElements/Input";
 
 import "./NewPlace.css";
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          // if inputId is equal to action.inputId, then check if action.isValid is true.
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          // if inputId is not equal to action.inputId, then check if state.inputs[inputId].isValid is true.
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          // action.inputId is the input ("title", "description", etc.) that is being changed.
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid, // formIsValid is either true or false.
+      };
+      default:
+        return state;
+  }
+};
+
 const NewPlace = () => {
+
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
   const titleInputHandler = useCallback((id, value, isValid) => {
 
   }, []);
