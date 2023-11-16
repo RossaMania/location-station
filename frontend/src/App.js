b4faca7e1cae38d25c141bb259f420eff4fcbf99
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-16 17:44:04 -0600

    Add login and logout functionality to App component with Context. Import AuthContext into App.js. Import useState into App.js. Wrap App with AuthContext.Provider component. Set isLoggedIn state var & setIsLoggedIn state func with useState hook. Create login func to set isLoggedIn state to true. Create logout func to set isLoggedIn state to false. Set value prop in AuthContext.Provider to isLoggedIn state context value, the login method to the login func, and the logout method to the logout func.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Outlet } from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
     <>
-      <MainNavigation />
-      <main>
-        <Outlet />
-      </main>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+        <MainNavigation />
+        <main>
+          <Outlet />
+        </main>
+      </AuthContext.Provider> 
     </>
   );
-
 }
 
 export default App;
