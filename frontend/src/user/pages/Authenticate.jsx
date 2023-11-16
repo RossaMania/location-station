9d7b04cad46bfe92ea99fafdda0142f352f781e9
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-15 20:23:22 -0600

    In Authenticate page, set up a form for email address and password using Input & Button components, and useForm hook. Add form validation and input fields.

--- frontend/src/user/pages/Authenticate.jsx
+++ frontend/src/user/pages/Authenticate.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/utils/validators';
+import Button from '../../shared/components/FormElements/Button';
+import { useForm } from '../../shared/hooks/form-hook';
 
 const Authenticate = () => {
+
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
   return (
-    <div>Authenticate</div>
-  )
+    <div>
+      <form>
+        <Input
+          id="email"
+          element="input"
+          type="text"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL(), VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid e-mail address."
+          onInput={inputHandler}
+        />
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(8)]}
+          errorText="Passwords must be at least 8 characters."
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default Authenticate
