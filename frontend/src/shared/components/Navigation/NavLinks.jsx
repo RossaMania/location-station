57f0f9af26c7b5dbe80bbccd3ba1d91d5cf1e8ac
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-16 17:57:50 -0600

    Import AuthContext from context folder into NavLinks component. Import useContext from react and set useContext hook to pass in the AuthContext props with auth const object.

--- frontend/src/shared/components/Navigation/NavLinks.jsx
+++ frontend/src/shared/components/Navigation/NavLinks.jsx
@@ -1,30 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import { AuthContext } from "../../context/auth-context";
 
 import "./NavLinks.css";
 
 const NavLinks = () => {
+  // useContext hook allows us to use the context in NavLinks without having to wrap the component in the context provider.
+  // auth is an object with isLoggedIn, login, and logout properties.
+  const auth = useContext(AuthContext);
+
   return (
     <ul className="nav-links">
       <li>
-        <NavLink to="/">
-          ALL USERS
-        </NavLink>
+        <NavLink to="/">ALL USERS</NavLink>
       </li>
       <li>
-        <NavLink to="/u1/places">
-          MY PLACES
-        </NavLink>
+        <NavLink to="/u1/places">MY PLACES</NavLink>
       </li>
       <li>
-        <NavLink to="/places/new">
-          ADD PLACE
-        </NavLink>
+        <NavLink to="/places/new">ADD PLACE</NavLink>
       </li>
       <li>
-        <NavLink to="/auth">
-          AUTHENTICATE
-        </NavLink>
+        <NavLink to="/auth">AUTHENTICATE</NavLink>
       </li>
     </ul>
   );
