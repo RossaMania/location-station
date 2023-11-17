f3dfbb1b7b5f96f4264ac51df94589deb2e041e5
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-17 14:25:22 -0600

    Import useNavigate from React Router into auth-hook.jsx. Set navigate const to call the useNavigate hook. Use navigate in login func to go to home page. Use navigate in logout func to go to /auth page. Create PrivateRoutes func component in shared/components/Navigation.

--- frontend/src/shared/hooks/auth-hook.jsx
+++ frontend/src/shared/hooks/auth-hook.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useCallback, useContext, useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext({
   isLoggedIn: false,
@@ -10,15 +11,19 @@ export const AuthProvider = ({children}) => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const navigate = useNavigate();
+
   // call this function when you want to authenticate the user
   const login = useCallback(() => {
     setIsLoggedIn(true);
-  }, []);
+    navigate("/"); // navigate to the home page
+  }, [navigate]);
 
   // call this function to sign out logged in user
   const logout = useCallback(() => {
     setIsLoggedIn(false);
-  }, []);
+    navigate("/auth"); // navigate to the authentication page
+  }, [navigate]);
 
 
   return (
