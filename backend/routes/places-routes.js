d8e7a5d207e933bed27086be6c2f0c280f16bacc
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-20 17:01:21 -0600

    Set the path filter for placesRoutes in server.js to "/api/places". Paste DUMMY_PLACES array into places-routes.js. Create placeId const to get the placeId from URL with params. Create place const that uses the find method on DUMMY_PLACES array to return an id that maches placeId. Set res.json to return the matching place data.

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,41 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-console.log("GET Request in Places Route!");
-res.json({message: "It works!"})
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId; // get the place ID from the URL.
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId; // find the place with the id of placeId.
+  });
+  res.json({ place: place }); // return the place.
 });
 
 module.exports = router;
