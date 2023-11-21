5f047a66fd701a52790f418840e42cf4db96198c
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-20 18:00:33 -0600

    Add users routes to backend. Pasting USERS array with additional dummy data, bringing in express & router all in users-routes.js. Create userId const to get the user ID from URL, and create user const to use the find method on USERS array, & get the user with the id that matches the userId. Set up a router handler in server.js with "/api/users" path filter and userRoutes const that requires /routes/users-routes.

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,13 @@ const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 
+const usersRoutes = require("./routes/users-routes");
+
 const app = express();
 
 app.use("/api/places", placesRoutes); // => /api/places/...
 
-
-
-
-
-
+app.use("/api/users", usersRoutes); // => /api/users/...
 
 
 app.listen(5000);
