3a217d8962f51c92a9a22bbb05cc66f057f87ea6
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-22 17:05:33 -0600

    Create controllers folder in backend. Create users-controllers.js & places-controllers.js files. Cut/paste DUMMY_DATA array into places-controllers. Cut/paste the route handler funcs into places-controllers and name them getPlaceById & getPlaceByUserId.

---
+++ backend/controllers/places-controllers.js
@@ -0,0 +1,54 @@
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.placeId; // get the place ID from the URL.
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId; // find the place with the id of placeId.
+  });
+
+  if (!place) {
+    throw new HttpError("Oops! No place found with that ID!", 404);
+  }
+
+  res.json({ place: place }); // return the place.
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.userId; // get the user ID from the URL.
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId; // find the user with the id of userId.
+  });
+
+  if (!place) {
+    return next(new HttpError("Oops! No place found with that user ID!", 404));
+  }
+
+  res.json({ place: place }); // return the user.
+};
