87e3cee0769d5749b6428122682dbae1e1a25222
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-25 19:47:26 -0600

    Install axios. Require axios in backend/util/location.js. Create getCoordinatesForAddress async func in location.js to take an address as an argument & return the geographical coordinates for that address using the Google Maps Geocoding API. If the API doesn't return any data, or the status of data is "ZERO_RESULTS", an error is thrown.

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,1 +1,28 @@
+import HttpError from "../models/http-error";
+
+const axios = require("axios");
+
 const apiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
+export const getCoordinatesForAddress = async (address) => {
+
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`
+  );
+
+  const data = response.data;
+
+// The address the user entered passed our validation, but Google Maps can't find it.
+// This may occur if the geocoder was passed a non-existent address.
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError("Oops! Could not find location for the specified address!", 422);
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+
+}
+
+module.exports = getCoordinatesForAddress;
