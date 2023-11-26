d9ced74a08f5a3b77b3c6088dc24db01594ce1ff
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-25 21:34:59 -0600

    Update Google Maps API key variable key name in location.js. Add console log in location.js

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,18 +2,19 @@ const axios = require("axios");
 
 const HttpError = require("../models/http-error");
 
-const apiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
 
 const getCoordinatesForAddress = async (address) => {
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
-    )}&key=${apiKey}`,
+    )}&key=${API_KEY}`,
     { headers: { "accept-encoding": "*" } }
   );
 
   const data = response.data;
+  console.log(response.data)
 
   // The address the user entered passed our validation, but Google Maps can't find it.
   // This may occur if the geocoder was passed a non-existent address.
