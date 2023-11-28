3eeefa441e031264c109ff5c7b4fb234faffd740
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-28 13:15:59 -0600

    Create user.js file in models folder. Create userSchema in user.js. Force install mongoose-unique-validator. Apply the uniqueValidator plugin to userSchema in user.js. Export User model using the userSchema.

---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const uniqueValidator = require("mongoose-unique-validator"); // import mongoose-unique-validator to use unique validator.
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true }, // name is a required string.
+  email: { type: String, required: true, unique: true }, // email is a required string. It's also unique which means it's indexed.
+  password: { type: String, required: true, minlength: 6 }, // password is a required string. It's also at least 6 characters long.
+  imageUrl: { type: String, required: true }, // imageURL is a required string. It's just a url to an image.
+  places: { type: String, required: true }, // places is a required string.
+})
+
+// Apply the uniqueValidator plugin to userSchema.
+// The unique validator will check for duplicate database entries and report them just like any other validation error.
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema); // export the userSchema as a model named "User".
