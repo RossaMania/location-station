ea17bfae412451a371af232fcb4356bcb654f617
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-30 17:39:19 -0600

    Refactor users-controllers.js file by console logging error & returning error in login func's if check.

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -6,22 +6,21 @@ const HttpError = require("../models/http-error");
 
 const User = require("../models/user");
 
-
 const getUsers = async (req, res, next) => {
-
   let users;
 
   try {
     users = await User.find({}, "-password"); // find all users in the database, but don't return the password field.
   } catch (err) {
-    const error = new HttpError("Oops! Something went wrong! Please try again later!", 500);
+    const error = new HttpError(
+      "Oops! Something went wrong! Please try again later!",
+      500
+    );
     console.log(err);
     return next(error);
   }
 
-  res.json({ users: users.map(user => user.toObject({ getters: true })) }); // return the users.
-
-
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) }); // return the users.
 };
 
 const getUserById = (req, res, next) => {
@@ -37,10 +36,9 @@ const signup = async (req, res, next) => {
 
   if (!errors.isEmpty()) {
     console.log(errors);
-    return next(new HttpError(
-      "Oops! Invalid inputs passed! Please check your data!",
-      422
-    ));
+    return next(
+      new HttpError("Oops! Invalid inputs passed! Please check your data!", 422)
+    );
   } // check if there are any validation errors.
 
   const { name, email, password } = req.body; // get the data from the request body.
@@ -50,23 +48,28 @@ const signup = async (req, res, next) => {
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Oops! Signing up failed! Please try again later!", 500);
-    console.log(err)
+    const error = new HttpError(
+      "Oops! Signing up failed! Please try again later!",
+      500
+    );
+    console.log(err);
     return next(error);
   }
 
   if (existingUser) {
-    const error = new HttpError("Oops! User already exists! Please login instead!", 422);
+    const error = new HttpError(
+      "Oops! User already exists! Please login instead!",
+      422
+    );
     return next(error);
   }
-
 
   const createdUser = new User({
     name: name,
     email: email,
     password: password, // this will be encrypted later.
     imageUrl: "https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg", // this will be a url to an image.
-    places: []
+    places: [],
   });
 
   try {
@@ -80,30 +83,35 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-
   res.status(201).json({ user: createdUser.toObject({ getters: true }) }); // return the created user.
 };
 
 const login = async (req, res, next) => {
-
   const { email, password } = req.body; // get the data from the request body.
 
-   let existingUser;
+  let existingUser;
 
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError("Oops! Logging in failed! Please try again later!", 500);
-    console.log(err)
+    const error = new HttpError(
+      "Oops! Logging in failed! Please try again later!",
+      500
+    );
+    console.log(err);
     return next(error);
   }
 
   if (!existingUser || existingUser.password !== password) {
-    const error = new HttpError("Oops! Invalid e-mail address OR password! Please try again!", 401);
+    const error = new HttpError(
+      "Oops! Invalid e-mail address OR password! Please try again!",
+      401
+    );
+    console.log(error);
+    return next(error);
   }
 
-res.json({ message: "Yay! Logged in successfully!"});
-
+  res.json({ message: "Yay! Logged in successfully!" });
 };
 
 exports.getUsers = getUsers;
