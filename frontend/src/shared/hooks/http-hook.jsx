7ec1ca4544637bcf6df35f9977a0c6bc03d0c2b0
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-30 20:56:24 -0600

    Add abort controller to cancel active HTTP requests if the user navigates away from the page before the request is complete and a useEffect cleanup function that runs when the component using the useHttpClient hook unmounts. The cleanup func loops through the activeHttpRequests array and aborts each request using the AbortController object.

--- frontend/src/shared/hooks/http-hook.jsx
+++ frontend/src/shared/hooks/http-hook.jsx
@@ -1,8 +1,10 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
 
   const sendRequest = useCallback(async (
     url,
@@ -12,11 +14,19 @@ export const useHttpClient = () => {
   ) => {
     setIsLoading(true);
 
+    // Create a new AbortController object.
+    // This will allow us to abort the request if the user navigates away from the page before the request is complete.
+    const httpAbortController = new AbortController();
+
+    activeHttpRequests.current.push(httpAbortController); // Add the AbortController object to the activeHttpRequests array so we can access it later.
+
     try {
       const response = await fetch(url, {
         method,
         headers,
         body,
+        // Pass the AbortController's signal to the fetch request so we can abort the request if the user navigates away from the page before the request is complete.
+        signal: httpAbortController.signal,
       });
 
       const responseData = await response.json();
@@ -27,7 +37,6 @@ export const useHttpClient = () => {
       }
 
       return responseData;
-
     } catch (err) {
       console.log(err);
       setError(
@@ -39,7 +48,15 @@ export const useHttpClient = () => {
 
   const clearError = () => {
     setError(null);
-  }
+  };
+
+  useEffect(() => {
+    return () => { // This will run when the component using this hook unmounts.
+      activeHttpRequests.current.forEach(abortController => // Loop through the activeHttpRequests array and abort each request.
+        abortController.abort() // Abort the request using the AbortController object.
+      );
+    };
+  }, []);
 
   return { isLoading: isLoading, error: error, sendRequest: sendRequest, clearError: clearError};
 };
