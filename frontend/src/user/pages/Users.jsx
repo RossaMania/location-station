b61b79d991860afde0d34ddcbc104f741d3d3af4
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-30 19:20:52 -0600

    Remove hard-coded USERS array from frontend/src/user/pagesUsers component. Import useEffect into Users component. Create async/await sendRequest func to fetch list of users.

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import UsersList from '../components/UsersList'
+import React, { useEffect } from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
+  // useEffect() runs once when the component is mounted.
+  // The empty array as the second argument ensures that useEffect() only runs once.
+  // If the second argument is not provided, useEffect() will run every time the component is updated.
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await fetch("http://localhost:5000/api/users");
 
-  const USERS = [
-    {
-      id: "u1",
-      name: "Ricky Blowtorch",
-      image:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2Fe410f6b3-617c-4210-b03a-793d69623fad%2Fddoaaml-b6fc78f4-6e91-4871-9dbc-3e9d598e2862.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2U0MTBmNmIzLTYxN2MtNDIxMC1iMDNhLTc5M2Q2OTYyM2ZhZFwvZGRvYWFtbC1iNmZjNzhmNC02ZTkxLTQ4NzEtOWRiYy0zZTlkNTk4ZTI4NjIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Brk0FtuamlhfyalfunOPwsDo9SJ4I9q_9CVW-0_bFn0&tbnid=P1ummVfhGZzWvM&vet=12ahUKEwjnyeuWpbOCAxU6NN4AHcj4D8QQMygTegQIARBa..i&imgrefurl=https%3A%2F%2Fwww.deviantart.com%2Fcoolcsd1986%2Fart%2FTeenage-Mutant-Ninja-Turtles-1987-Version-826851405&docid=MNyTHM8PRrordM&w=1000&h=1300&q=teenage%20mutant%20ninja%20turtles%20deviantart&ved=2ahUKEwjnyeuWpbOCAxU6NN4AHcj4D8QQMygTegQIARBa",
-      places: 69,
-    },
-    {
-      id: "u2",
-      name: "Hannah Steele",
-      image:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2Fe410f6b3-617c-4210-b03a-793d69623fad%2Fddoaaml-b6fc78f4-6e91-4871-9dbc-3e9d598e2862.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2U0MTBmNmIzLTYxN2MtNDIxMC1iMDNhLTc5M2Q2OTYyM2ZhZFwvZGRvYWFtbC1iNmZjNzhmNC02ZTkxLTQ4NzEtOWRiYy0zZTlkNTk4ZTI4NjIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Brk0FtuamlhfyalfunOPwsDo9SJ4I9q_9CVW-0_bFn0&tbnid=P1ummVfhGZzWvM&vet=12ahUKEwjnyeuWpbOCAxU6NN4AHcj4D8QQMygTegQIARBa..i&imgrefurl=https%3A%2F%2Fwww.deviantart.com%2Fcoolcsd1986%2Fart%2FTeenage-Mutant-Ninja-Turtles-1987-Version-826851405&docid=MNyTHM8PRrordM&w=1000&h=1300&q=teenage%20mutant%20ninja%20turtles%20deviantart&ved=2ahUKEwjnyeuWpbOCAxU6NN4AHcj4D8QQMygTegQIARBa",
-      places: 420,
-    },
-  ];
-  return <UsersList items={USERS} />
-}
+      const responseData = await response.json();
+    };
+    sendRequest();
+  }, []);
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
