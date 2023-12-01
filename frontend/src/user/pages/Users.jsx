939950502eef284120f055369d9fd7421075bd32
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-30 19:44:04 -0600

    Add some error handling ion sendRequest func in Users component. Try-catch block, with data fetching in the try, and err console log & setError state func called with err.message in the catch. setIsLoading called and set to true above the try-catch & then set to false below the try-catch. If check set in the try block to throw a new Error JS object if response from data fetching isn't ok.

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
   const [loadedUsers, setLoadedUsers] = useState();
@@ -10,12 +9,21 @@ const Users = () => {
   useEffect(() => {
     const sendRequest = async () => {
       setIsLoading(true);
-  
-      const response = await fetch("http://localhost:5000/api/users");
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
 
-      const responseData = await response.json();
+        const responseData = await response.json();
 
-      setLoadedUsers(responseData.users)
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err)
+        setError(err.message);
+      }
       setIsLoading(false);
     };
     sendRequest();
