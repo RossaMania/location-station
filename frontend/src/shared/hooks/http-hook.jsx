2fa65fe6826bcb8e6a4f345474a7053312d426bc
Ross Clettenberg <raclettenberg@gmail.com>
2023-11-30 20:34:29 -0600

    In sendRequest func of useHttpClient in http-hook.jsx, pass in url, method, body, & headers as arguments. Set method default to "GET", body default to null, & headers to empty object { }. Refactor the try-catch code from Authenticate.jsx into the useHttpClient named export, including error handling and loading state. Return thje isLoading state, the error state, and the sendRequest func. Import useCallback, & wrap sendRequest async-await func with it.

--- frontend/src/shared/hooks/http-hook.jsx
+++ frontend/src/shared/hooks/http-hook.jsx
@@ -1,10 +1,41 @@
-import {useState } from "react";
+import { useState, useCallback } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const sendRequest = () => {
-    
-  }
+  const sendRequest = useCallback(async (
+    url,
+    method = "GET",
+    body = null,
+    headers = {}
+  ) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url, {
+        method,
+        headers,
+        body,
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        console.log(responseData.message);
+        throw new Error(responseData.message);
+      }
+
+      return responseData;
+
+    } catch (err) {
+      console.log(err);
+      setError(
+        err.message || "Oops! Something went wrong! Please try again later!"
+      );
+    }
+    setIsLoading(false);
+  }, []);
+
+  return { isLoading: isLoading, error: error, sendRequest: sendRequest};
 };
