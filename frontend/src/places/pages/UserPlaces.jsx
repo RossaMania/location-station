341376d050223270aa01eccfe0e0e72ff34347dd
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-02 17:16:30 -0600

    Refactor UserPlaces component to handle loading and error states by importing ErrorModal & LoadingSpinner, and setting an empty fragment in the return statement. Then conditionally rendering the LoadingSpinner and PlaceList components.

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from "react";
 import PlaceList from "../components/PlaceList";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import { useParams } from "react-router-dom";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
-
   const [loadedPlaces, setLoadedPlaces] = useState();
 
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -15,7 +16,9 @@ const UserPlaces = () => {
   useEffect(() => {
     const fetchPlaces = async () => {
       try {
-        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
         setLoadedPlaces(responseData.places);
 
         console.log(responseData);
@@ -24,7 +27,18 @@ const UserPlaces = () => {
     fetchPlaces();
   }, [sendRequest, userId]);
 
-  return <PlaceList items={loadedPlaces} />;
-}
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
+  );
 
-export default UserPlaces
+};
+
+export default UserPlaces;
