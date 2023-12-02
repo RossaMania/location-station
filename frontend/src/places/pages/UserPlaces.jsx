fd8f9cbda90a36a5baf473eeae5730f75275d533
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-02 17:06:54 -0600

    Delete DUMMY_PLACES array in UserPlaces.jsx. Import useHttpClient custom hook into UserPlaces.jsx. Add useEffect hook to fetch user places with fetchPlaces async await func & a try catch block. Call useState hook with loadedPlaces state var & setLoadedPlaces state func to handle place data.

--- frontend/src/places/pages/UserPlaces.jsx
+++ frontend/src/places/pages/UserPlaces.jsx
@@ -1,42 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import PlaceList from "../components/PlaceList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import { useParams } from "react-router-dom";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644,
-    },
-    creator: "u2",
-  },
-];
 
 const UserPlaces = () => {
 
+  const [loadedPlaces, setLoadedPlaces] = useState();
+
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   let { userId } = useParams();
 
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        setLoadedPlaces(responseData.places);
+
+        console.log(responseData);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
 
   return <PlaceList items={loadedPlaces} />;
 }
