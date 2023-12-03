e828cbdddafae89dad9c74d0c7fdb04e7c04ebcb
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-03 11:10:17 -0600

    Add onClick listener to Pick Image Button. Set onClick to pickImageHandler func. Add file picker functionality to ImageUpload component by wiring up pickImageHandler to useRef with current filePickerRef const.

--- frontend/src/shared/components/FormElements/ImageUpload.jsx
+++ frontend/src/shared/components/FormElements/ImageUpload.jsx
@@ -1,18 +1,25 @@
-import React from "react"
+import React, { useRef } from "react"
 
 import Button from "./Button"
 
 import "./ImageUpload.css"
 
 const ImageUpload = (props) => {
+
+  const filePickerRef = useRef();
+
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  }
+
   return (
     <div className="form-control">
-      <input id={props.id} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg" />
+      <input id={props.id} ref={filePickerRef} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg" />
       <div className={`image-upload ${props.center && "center"}`}>
         <div className="image-upload__preview">
         <img src="" alt="Preview" />
         </div>
-        <Button type="button">PICK IMAGE</Button>
+        <Button type="button" onClick={pickImageHandler}>PICK IMAGE</Button>
       </div>
     </div>
   )
