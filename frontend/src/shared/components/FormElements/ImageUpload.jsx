cf0501b0a7ab4e324789e8b6664f67310f00a073
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-03 11:00:14 -0600

    Bring in ImageUpload.css from the course into src/chared/components/FormElements folder. Import ImageUpload.css into ImageUpload func component. Create file picker input, with image preview and pick image Button using Button component (after importing).

--- frontend/src/shared/components/FormElements/ImageUpload.jsx
+++ frontend/src/shared/components/FormElements/ImageUpload.jsx
@@ -1,8 +1,20 @@
 import React from "react"
 
-const ImageUpload = () => {
+import Button from "./Button"
+
+import "./ImageUpload.css"
+
+const ImageUpload = (props) => {
   return (
-    <div>ImageUpload</div>
+    <div className="form-control">
+      <input id={props.id} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg" />
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+        <img src="" alt="Preview" />
+        </div>
+        <Button type="button">PICK IMAGE</Button>
+      </div>
+    </div>
   )
 }
 
