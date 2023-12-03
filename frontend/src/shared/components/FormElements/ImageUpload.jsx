5cadc9cc97622e67727480d35018cf90cbe41947
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-03 11:18:13 -0600

    Add onChange listener prop to input of ImageUpload component. Wire it to pickedFileHandler func and console log the file input element--the file that's picked--in pickedFileHandler.

--- frontend/src/shared/components/FormElements/ImageUpload.jsx
+++ frontend/src/shared/components/FormElements/ImageUpload.jsx
@@ -8,13 +8,17 @@ const ImageUpload = (props) => {
 
   const filePickerRef = useRef();
 
+  const pickedFileHandler = event => {
+    console.log(event.target); // Console log the file input element
+  }
+
   const pickImageHandler = () => {
     filePickerRef.current.click();
   }
 
   return (
     <div className="form-control">
-      <input id={props.id} ref={filePickerRef} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg" />
+      <input id={props.id} ref={filePickerRef} style={{display: "none"}} type="file" accept=".jpg,.png,.jpeg" onChange={pickedFileHandler}/>
       <div className={`image-upload ${props.center && "center"}`}>
         <div className="image-upload__preview">
         <img src="" alt="Preview" />
