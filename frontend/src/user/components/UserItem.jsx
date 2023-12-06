51d9e8efac6d22b11a5aed52ebad36e5f5904f7d
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-05 20:48:12 -0600

    Correct classname in UserItem.jsx to user-item__image.

--- frontend/src/user/components/UserItem.jsx
+++ frontend/src/user/components/UserItem.jsx
@@ -11,7 +11,7 @@ const UserItem = (props) => {
     <li className="user-item">
         <Card className="user-item__content">
           <Link to={`/${props.id}/places`}>
-            <div className="user=item__image">
+            <div className="user-item__image">
               <Avatar image={`http://localhost:5000/${props.imageUrl}`} alt={props.name} />
             </div>
 
