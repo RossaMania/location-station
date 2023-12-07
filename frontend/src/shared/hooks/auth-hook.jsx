347eb6d213322f03510a3246d01ebfee55278bdd
Ross Clettenberg <raclettenberg@gmail.com>
2023-12-07 17:49:04 -0600

    Refactor auth-hook.jsx to improve readability and add token expiration countdown

--- frontend/src/shared/hooks/auth-hook.jsx
+++ frontend/src/shared/hooks/auth-hook.jsx
@@ -1,5 +1,13 @@
-import React, { createContext, useCallback, useContext, useEffect, useState } from "react"
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
+
+let logoutTimer;
 
 export const AuthContext = createContext({
   isLoggedIn: false,
@@ -9,28 +17,37 @@ export const AuthContext = createContext({
   logout: () => {},
 });
 
-export const AuthProvider = ({children}) => {
-
+export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(false);
+  // This is the countdown to the token expiration date.
+  const [tokenExpirationCountdown, setTokenExpirationCountdown] = useState();
   // const [isLoggedIn, setIsLoggedIn] = useState(false); Replaced by token, setToken.
   const [userId, setUserId] = useState(false);
 
   const navigate = useNavigate();
 
   // call this function when you want to authenticate the user
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token); // set the token
-    setUserId(uid); // set the user id
+  const login = useCallback(
+    (uid, token, expirationDate) => {
+      setToken(token); // set the token
+      setUserId(uid); // set the user id
 
-    const tokenExpirationDate =  expirationDate || new Date(
-      new Date().getTime() + 1000 * 60 * 60 * 24 * 2
-    ); // calculate the expiration date of the token
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({ userId: uid, token: token, expiration: tokenExpirationDate.toISOString() })
-    ); // store the token in the local storage
-    navigate("/"); // navigate to the home page
-  }, [navigate]);
+      const tokenExpirationDate =
+        expirationDate ||
+        new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 2); // calculate the expiration date of the token
+      setTokenExpirationCountdown(tokenExpirationDate);
+      localStorage.setItem(
+        "userData",
+        JSON.stringify({
+          userId: uid,
+          token: token,
+          expiration: tokenExpirationDate.toISOString(),
+        })
+      ); // store the token in the local storage
+      navigate("/"); // navigate to the home page
+    },
+    [navigate]
+  );
 
   // call this function to sign out logged in user
   const logout = useCallback(() => {
@@ -40,24 +57,47 @@ export const AuthProvider = ({children}) => {
     navigate("/auth"); // navigate to the authentication page
   }, [navigate]);
 
-   useEffect(() => {
-     //This stored data will be an object with the userId and token.
-     const storedData = JSON.parse(localStorage.getItem("userData"));
+  useEffect(() => {
+    if (token && tokenExpirationCountdown) {
+      const remainingTime = tokenExpirationCountdown.getTime() - Date.now();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    };
+  }, [token, logout, tokenExpirationCountdown]);
 
-     if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
-       // setIsLoggedIn(true); Replaced by setToken.
-       login(storedData.userId, storedData.token, new Date(storedData.expiration));
-     }
-   }, [login]);
+  useEffect(() => {
+    //This stored data will be an object with the userId and token.
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      // setIsLoggedIn(true); Replaced by setToken.
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn: !!token, token: token, userId: userId, login: login, logout: logout }}
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
     >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = () => {
   return useContext(AuthContext);
